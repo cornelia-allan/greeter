a6351d5315920ce0e10990cde07544fcd7e8c480
Jack Allan <Jack.Allan@fenergo.com>
2018-12-01 11:34:20 +0000

    Adding github pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      showName: false
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {!this.state.showName && (
+            <>
+              <label for="Name">Please enter your Name</label>
+              <input
+                name="Name"
+                onChange={event => this.setState({ name: event.target.value })}
+              />
+              <button onClick={() => this.setState({ showName: true })}>
+                Done
+              </button>
+            </>
+          )}
+          <p>{this.state.showName && hi(this.state.name)}</p>
         </header>
       </div>
     );
@@ -26,3 +36,7 @@ class App extends Component {
 }
 
 export default App;
+
+function hi(name) {
+  return "Hello " + name;
+}
